0f9f7422055c8025224258b0d1c672cacec727ab
devsandanayake <devinsandanayake49@gmail.com>
2023-09-05 10:50:34 +0530

    update

--- fontendside/src/component/Slidebar.jsx
+++ fontendside/src/component/Slidebar.jsx
@@ -53,8 +53,8 @@ export default function Slidebar({ onLogout }) {
             <div className="flex p-2 text-2xl"><BsTools className='mr-3 text-2xl'/>Tools</div>
           </div>
         </button>
-
-        <button className="w-44 ml-14" onClick={() => handleComponentChange('issuetools')}>
+        <div className='bg-white w-2  ml-9 mt-2 py-10 absolute'></div>
+       <button className="w-44 ml-14" onClick={() => handleComponentChange('issuetools')}>
           <div className='flex items-center rounded-md bg-blue-400 mt-2'>
             <div className="flex p-2 text-sm font-extrabold"><BsTools className='mr-3'/>Issue Tools</div>
           </div>
@@ -71,28 +71,28 @@ export default function Slidebar({ onLogout }) {
             <div className='flex p-2 text-2xl'><FaToolbox className='mr-3 text-2xl'/>Move Items</div>
           </div>
         </button>
-
-        <button className="w-44 ml-14 mb-10" onClick={() => handleComponentChange('issueitems')}>
+        <div className='bg-white w-2  ml-9 mt-2 py-16 absolute'></div>
+        <button className="w-44 ml-14" onClick={() => handleComponentChange('issueitems')}>
           <div className='flex items-center rounded-md bg-green-400 mt-2'>
             <div className="flex p-2 text-sm font-extrabold"><FaToolbox className='mr-3'/>Issue Items</div>
           </div>
         </button>
 
-        <button className="w-60" onClick={() => handleComponentChange('project')}>
+        <button className="w-44 ml-14" onClick={() => handleComponentChange('project')}>
           <div className='flex items-center rounded-md bg-blue-600 mt-2'>
-          <div className='flex p-2 text-2xl'><AiFillProfile className='mr-3 text-2xl'/>Projects</div>
+          <div className='flex p-2 text-sm font-extrabold'><AiFillProfile className='mr-3'/>Projects</div>
           </div>
         </button>
 
-        <button className="w-60" onClick={() => handleComponentChange('returnItem')}>
-          <div className='flex items-center rounded-md bg-red-600 mt-2'>
-          <div className='flex p-2 text-2xl'><AiFillBug className='mr-3 text-2xl'/>Return Items</div>
+        <button className="w-44 ml-14" onClick={() => handleComponentChange('returnItem')}>
+          <div className='flex items-center rounded-md bg-pink-600 mt-2'>
+          <div className='flex p-2 text-sm font-extrabold'><AiFillBug className='mr-3'/>Return Items</div>
           </div>
         </button>
 
       
         <button  onClick={onLogout} className='ml-16 mt-16'>
-        <div className='flex items-center rounded-md bg-green-500 mt-2'>
+        <div className='flex items-center rounded-md bg-red-500 mt-2'>
           <div className='flex p-2 font-bold text-2xl'><AiOutlineLogout className='mr-3 text-2xl'/>Logout</div>
           </div>
         </button>
